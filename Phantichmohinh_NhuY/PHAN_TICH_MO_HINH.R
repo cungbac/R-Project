160a06244c8f913b049c87b384eff0af67830bb4
unknown <1554030261y@ou.edu.vn>
2020-03-11 22:37:32 +0700

    Machine learning homework

---
+++ Phantichmohinh_NhuY/PHAN_TICH_MO_HINH.R
@@ -0,0 +1,60 @@
+# Library
+library(caret)
+library(tidyverse)
+library(mlbench) # To employ BostonHousing data
+library(leaps) # To apply stepwise, backward and forward regression
+# Data
+data("BostonHousing")
+head(BostonHousing)
+str(BostonHousing)
+summary(BostonHousing)
+?BostonHousing
+# Data split: train and test data
+train_index<-createDataPartition(BostonHousing$medv,p=0.7,list = F)
+train<-BostonHousing[train_index,]
+test<- BostonHousing[-train_index,]
+# Train control
+ctr <- trainControl(method = "repeatedcv",number = 2,repeats = 5)
+# Train models
+##Model base
+model_base = train(medv~chas,
+           data = BostonHousing,
+           method = 'lm',
+           trControl = ctr)
+##Model dummy
+model_dummy = lm(medv~chas,data = train) #dummy la 1 va 0
+##Model Interaction
+model_interaction = lm(medv~chas + lstat + chas:lstat,train)
+##Model Stepwise
+model_stepwise = train(medv~., 
+                       data = train,
+                       method = "leapSeq",
+                       tuneGrid = data.frame(nvmax = 1:5),
+                       trControl = ctr)
+##Model Backward
+model_backward = train(medv~., 
+                       data = train,
+                       method = "leapBackward",
+                       tuneGrid = data.frame(nvmax = 1:5),
+                       trControl = ctr)
+##Model Forward
+model_forward = train(medv~., 
+                       data = train,
+                       method = "leapForward",
+                       tuneGrid = data.frame(nvmax = 1:5),
+                       trControl = ctr)
+# Explore the results: 6 final models
+model_base$finalModel
+model_dummy
+model_interaction
+summary(model_stepwise$finalModel)
+summary(model_backward$finalModel)
+summary(model_forward$finalModel)
+# Calculate fitted values of 6 final models and add them into dataframe
+base.fit <- fitted(model_base)
+dummy.fit <- fitted(model_dummy)
+interaction.fit <- fitted(model_interaction)
+step.fit <- fitted(model_stepwise)
+back.fit <- fitted(model_backward)
+for.fit <- fitted(model_forward)
+data.fit <- data.frame(base.fit,dummy.fit,interaction.fit,step.fit,back.fit,for.fit)
